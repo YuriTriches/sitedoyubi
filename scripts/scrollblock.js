63fdbd80709b64d3acafaf585b04d4b0c124217b
Devys <145410373+YuriTriches@users.noreply.github.com>
2026-04-27 17:52:43 -0300

    Delete scripts directory

--- scripts/scrollblock.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("mousemove", () => {
-  document.querySelectorAll("*").forEach(el => {
-    if (el.scrollWidth > el.clientWidth) {
-      el.style.outline = "2px solid red";
-    }
-  });
-});
